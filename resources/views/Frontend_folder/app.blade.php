e8c5b56d8b20b25fc0382b740b66eff448a6f057
ajayhoma <sharma.ajaya2019@gmail.com>
2024-03-20 22:04:43 +0545

    inital commit

---
+++ resources/views/Frontend_folder/app.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <!-- ... -->
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    <!-- ... -->
+</head>
+
+<body>
+    <!-- ... -->
+    <script src="{{ mix('js/app.js') }}"></script>
+</body>
+
+
+
+</html>
